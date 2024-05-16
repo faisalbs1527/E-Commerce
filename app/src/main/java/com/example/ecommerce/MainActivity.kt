c4ac0c56431921d960dbf89c0f57302963a37dc5
faisal Ahammed <faisal.ahamed@brainstation-23.com>
2024-05-16 17:26:23 +0600

    Bottom Navigation Added

--- app/src/main/java/com/example/ecommerce/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce/MainActivity.kt
@@ -5,16 +5,53 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ecommerce.databinding.ActivityMainBinding
+import com.example.ecommerce.screen.category.CategoryFragment
 import com.example.ecommerce.screen.home.HomeFragment
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val fragmentManager = supportFragmentManager
         val transaction = fragmentManager.beginTransaction()
         transaction.replace(R.id.frame_layout, HomeFragment())
         transaction.commit()
 
+        binding.bottomNavigationView.setOnItemSelectedListener{ item ->
+            when(item.itemId){
+                R.id.menu_home -> {
+
+                    if(supportFragmentManager.findFragmentById(R.id.homeFragment) == null){
+                        supportFragmentManager.beginTransaction().
+                                replace(R.id.frame_layout,HomeFragment()).commit()
+                    }
+                    true
+                }
+                R.id.menu_category -> {
+                    if(supportFragmentManager.findFragmentById(R.id.categoryFragment) == null){
+                        supportFragmentManager.beginTransaction().
+                        replace(R.id.frame_layout,CategoryFragment()).commit()
+                    }
+                    true
+                }
+                R.id.menu_search ->{
+                    true
+                }
+                R.id.menu_account ->{
+                    true
+                }
+                R.id.menu_more ->{
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
