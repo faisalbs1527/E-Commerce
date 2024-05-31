e36846a612b1c051660690085fb476f843d4bc03
faisal Ahammed <faisal.ahamed@brainstation-23.com>
2024-05-31 18:49:06 +0600

    Card Added

--- app/src/main/java/com/example/ecommerce/screen/CheckoutFragment.kt
+++ app/src/main/java/com/example/ecommerce/screen/CheckoutFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,29 +19,39 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -54,6 +65,7 @@ import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -85,6 +97,8 @@ class CheckoutFragment : Fragment(R.layout.fragment_checkout) {
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun Message() {
+
+        var text by remember { mutableStateOf("") }
 
         Scaffold(
             topBar = {
@@ -132,17 +146,55 @@ class CheckoutFragment : Fragment(R.layout.fragment_checkout) {
                     .padding(innerPadding),
                 verticalArrangement = Arrangement.spacedBy(15.dp),
             ) {
-                Text(
-                    modifier = Modifier.padding(8.dp),
-                    text =
-                    """
-                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
+                OutlinedCard(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(innerPadding)
+                        .padding(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 16.dp),
+                    shape = RoundedCornerShape(1.dp),
+                    elevation = CardDefaults.cardElevation(16.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color.White)
+                ) {
 
-                    It also contains some basic inner content, such as this text.
+                    Box(modifier = Modifier
+                        .fillMaxWidth()
+                        .background(brush = gradientColor())
+                    )
+                    {
+                        Text(
+                            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                            text = "Billing Address",
+                            color = Color.White
+                        )
+                    }
+                    Text(
+                        modifier = Modifier.padding(top = 16.dp, start = 16.dp),
+                        text = "Address",
+                        fontWeight = FontWeight.Bold
+                    )
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 12.dp, vertical = 4.dp),
+                        value = text,
+                        onValueChange = {text = it},
+                        label = {Text(
+                            "Existing Address:",
+                            fontSize = 12.sp,
+                            color = Color(0xFF7D828B)
+                        )},
+                        colors = TextFieldDefaults.colors(
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedContainerColor = Color.Transparent,
+                            focusedIndicatorColor = Color(0xFFCDD1D4),
+                            unfocusedIndicatorColor  = Color(0xFFCDD1D4)
+                        ),
+                        trailingIcon = {
+                            Icon(Icons.Filled.ArrowDropDown, contentDescription = null)
+                        }
+                    )
+                }
 
-                    You have pressed the floating action button times.
-                """.trimIndent(),
-                )
             }
         }
     }
